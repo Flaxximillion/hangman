4af9a529ed14e8c27662f6392c228637adfee290
Flaxximillion <ayu2013@gmail.com>
2017-06-22 17:29:04 -0500

    IT WORKS BETTER NOW

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 /* *
-*
-*
-* SUPER IMPORTANT NOTES
-* "let" and "const" are replacements for "var". Don't worry about them.
-*
-* Any extraneous code that doesn't have to do with the base logic of the game will be marked. Ignore those too
-*
-* */
+ *
+ *
+ * SUPER IMPORTANT NOTES
+ * "let" and "const" are replacements for "var". Don't worry about them.
+ *
+ * Any extraneous code that doesn't have to do with the base logic of the game will be marked. Ignore those too
+ *
+ * */
 
 
 //vvvv--IMPORTANT STUFF--vvvv--IMPORTANT STUFF--vvvv--IMPORTANT STUFF--vvvv--IMPORTANT STUFF--vvvv//
@@ -14,35 +14,34 @@
 //
 //Declare all essential global variables here
 //
-//vvvv--IMPORTANT STUFF--vvvv--IMPORTANT STUFF--vvvv--IMPORTANT STUFF--vvvv--IMPORTANT STUFF--vvvv//
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!//
 
-    //Get the div that holds all the letters.
+//Get the div that holds all the letters.
 
 let letterHolder = document.getElementById("letterHolder");
 
-    //Create the word variable globally.
+//Create the word variable globally.
 
 let word;
 
-    //Create the array of word characters.
+//Create the array of word characters.
 
 let charArray = [];
 
-    //Create the array of what the user has guessed.
+//Create the array of what the user has guessed.
 
 let userGuesses = [];
 
-    //Set the amount of guesses the user start with.
+//Set the amount of guesses the user start with.
 
 let guessesRemaining = 6;
 
-    //Create the counter for the number of characters the users has guessed correctly.
+//Create the counter for the number of characters the users has guessed correctly.
 
 let charCounter = 0;
 
 
-    //Get the start button.
+//Get the start button.
 
 let buttonStart = document.getElementById("startPlay");
 
@@ -51,13 +50,6 @@ let buttonStart = document.getElementById("startPlay");
 let guessSpan = document.getElementById("youveGuessed");
 
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!//
-//^^^^--IMPORTANT STUFF--^^^^--IMPORTANT STUFF--^^^^IMPORTANT STUFF^^^^--IMPORTANT STUFF--^^^^//
-//
-//Declare all essential global variables here
-//
-//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!//
-//^^^^--IMPORTANT STUFF--^^^^--IMPORTANT STUFF--^^^^IMPORTANT STUFF^^^^--IMPORTANT STUFF--^^^^//
-
 
 
 
@@ -66,15 +58,22 @@ let guessSpan = document.getElementById("youveGuessed");
 //
 //
 //These variables are not for the game logic. You can ignore them.
-//
-//
-//vvvvvvvvvvvvvvvvvvUNIMPORTANTvvvvvvvvvvvvvvvvvvvvvvvvv//
 
-    //Get the div that displays the loading circle.
+
+//Get the div that displays the loading circle.
 
 let loadingDiv = document.getElementById("loader");
 
-    //Set the position for each piece of the canvas.
+//Set an arbitrary load time to give time for the first cat image to load because I am
+//too lazy to use an async function :^)
+window.onload = function(){
+    window.setTimeout(function(){
+        buttonStart.classList.remove("invisible");
+        loadingDiv.classList.add("invisible");
+    },3000);
+};
+
+//Set the position for each piece of the canvas.
 
 const imagePieces = {
     5: [0, 0],
@@ -85,37 +84,29 @@ const imagePieces = {
     0: [2, 1]
 };
 
-    //Get the canvas and set the base image variables.
+//Get the canvas and set the base image variables.
 
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext('2d');
 let image = new Image();
-image.onerror = getImage();
+
 let image2 = new Image();
 
 //^^^^^^^^^^^^^^^^^^^UNIMPORTANT^^^^^^^^^^^^^^^^^^^^^^//
-//
-//
-//These variables are not for the game logic. You can ignore them.
-//
-//
-//^^^^^^^^^^^^^^^^^^^UNIMPORTANT^^^^^^^^^^^^^^^^^^^^^^//
-
 
 
 
 
 
 //vvvv--IMPORTANT STUFF--vvvv--IMPORTANT STUFF--vvvv--IMPORTANT STUFF--vvvv--IMPORTANT STUFF--vvvv//
+
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!//
 //
 //This is the code that starts the game when the "Start Game!" button is pressed.
 //
-//vvvv--IMPORTANT STUFF--vvvv--IMPORTANT STUFF--vvvv--IMPORTANT STUFF--vvvv--IMPORTANT STUFF--vvvv//
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!//
 
-
-buttonStart.onclick=function(){
+buttonStart.onclick = function () {
 
     //First, we make the button invisible and change the class
     //so we can reuse it to restart the game later.
@@ -136,24 +127,25 @@ buttonStart.onclick=function(){
 //After a working image is fully loaded, the rest of the game will start.
 //This is unessential and not important unless you want cool pictures.
 
-function genCatPic(){
 
-    //vvvvvvvvvvvvvUNIMPORTANTvvvvvvvvvvvvvv//
-    loadingDiv.classList.remove("invisible");
-    image.onload = function(){
+function genCatPic() {
+    image.onload = function () {
         loadingDiv.classList.add("invisible");
         setCanvas();
-    //^^^^^^^^^^^^^UNIMPORTANT^^^^^^^^^^^^^//
+        //^^^^^^^^^^^^^UNIMPORTANT^^^^^^^^^^^^^//
 
 
         //This will run the next important functions.
         //If you weren't waiting for an image to load like I am,
         //You can call gameInit() and genWordDiv() when the player
         //clicks the start button instead of genCatPic().
-
         gameInit();
+        image.onload = null;
         genWordDiv();
     };
+
+    //vvvvvvvvvvvvvUNIMPORTANTvvvvvvvvvvvvvv//
+    loadingDiv.classList.remove("invisible");
 
     //vvvvvvvvvvvvvUNIMPORTANTvvvvvvvvvvvvvv//
     getImage();
@@ -175,13 +167,13 @@ function genWord() {
 //It will give whatever key the user pressed to the function "keyFunctions".
 
 
-function gameInit(){
+function gameInit() {
     window.addEventListener("keypress", keyFunctions);
 }
 
 //This takes whatever key the event listener gives it and parses it.
 
-function keyFunctions(){
+function keyFunctions() {
 
     //I have a div that tells you the current status of the game.
     //This will reset it to be blank when the user presses a new key.
@@ -253,10 +245,10 @@ function checkKey(key) {
     //already pressed.
     //If the user has already guessed the key they pressed, take away a guess, and tell them they suck.
 
-    if (userGuessIndex === -1){
+    if (userGuessIndex === -1) {
         userGuesses.push(key);
         guessSpan.textContent = guessSpan.textContent + " " + key.toUpperCase();
-    } else if(userGuessIndex !== -1) {
+    } else if (userGuessIndex !== -1) {
         guessesRemaining--;
         setGameStatus(1);
         return;
@@ -266,10 +258,10 @@ function checkKey(key) {
     //the index of characters the user guessed correctly. Keep in mind that this array will be
     //recreated each time this function runs.
 
-    if (charIndex!== -1) {
+    if (charIndex !== -1) {
         let index = [];
-        for(i = 0; i< charArray.length; i++){
-            if(charArray[i] === key){
+        for (i = 0; i < charArray.length; i++) {
+            if (charArray[i] === key) {
                 index.push(i);
             }
         }
@@ -278,13 +270,13 @@ function checkKey(key) {
         //the appropriate div to reveal.
         //Also if the user has guessed all the characters, set the game status to 5, aka "win".
 
-        for(j=0; j < index.length; j++){
+        for (j = 0; j < index.length; j++) {
             let divToShow = document.getElementById("letter_" + index[j]);
             divToShow.classList.remove("invisible");
             charCounter++;
-                if(charCounter === charArray.length){
-                    setGameStatus(5);
-                }
+            if (charCounter === charArray.length) {
+                setGameStatus(5);
+            }
         }
     }
 
@@ -305,7 +297,7 @@ function checkKey(key) {
 //setGameStatus(4) is called to reset the text between key presses.
 //If the user wins, tell them they win and run the function to end the game ("5").
 
-function setGameStatus(set){
+function setGameStatus(set) {
 
     //Get the div that displays the game status.
 
@@ -316,21 +308,21 @@ function setGameStatus(set){
     let gameGuesses = document.getElementById("guesses");
     gameGuesses.textContent = guessesRemaining;
 
-    if(guessesRemaining === 0){
-        setDiv.textContent="You lost!";
+    if (guessesRemaining === 0) {
+        setDiv.textContent = "You lost!";
         setGameImage();
         gameEnd();
-    } else if(set === 1){
-        setDiv.textContent= "You've already guessed that!";
+    } else if (set === 1) {
+        setDiv.textContent = "You've already guessed that!";
         setGameImage();
-    } else if (set === 2){
-        setDiv.textContent= "Nope.jpg";
+    } else if (set === 2) {
+        setDiv.textContent = "Nope.jpg";
         setGameImage();
-    } else if (set === 3){
-        setDiv.textContent= "yay";
-    } else if (set === 4){
+    } else if (set === 3) {
+        setDiv.textContent = "yay";
+    } else if (set === 4) {
         setDiv.textContent = "";
-    } else if (set === 5){
+    } else if (set === 5) {
         setDiv.textContent = "Win!";
         gameEnd();
     }
@@ -338,11 +330,11 @@ function setGameStatus(set){
 
 //Function that executes when the game ends.
 
-function gameEnd(){
+function gameEnd() {
 
     //Show the user the full word.
 
-    for(i = 0; i < charArray.length; i++){
+    for (i = 0; i < charArray.length; i++) {
         let show = document.getElementById("letter_" + i);
         show.classList.remove("invisible");
     }
@@ -362,14 +354,14 @@ function gameEnd(){
     //give the restart function the restart button because I'm too lazy to
     //set it to a global variable.
 
-    restartButton.onclick = function(){
+    restartButton.onclick = function () {
         restart(this);
     };
 }
 
 //Function to restart the game.
 
-function restart(button){
+function restart(button) {
 
     //Make the button invisible
 
@@ -377,7 +369,7 @@ function restart(button){
 
     //Not important, but this clears my HTML canvas.
 
-    ctx.clearRect(0,0,canvas.width,canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     //Reset the number of guesses remaining.
 
@@ -393,8 +385,8 @@ function restart(button){
 
     guessSpan.textContent = "";
     charArray = [];
-    userGuesses=[];
-    charCounter=0;
+    userGuesses = [];
+    charCounter = 0;
 
     //Call the first function necessary to start the game again.
 
@@ -405,13 +397,12 @@ function restart(button){
 //^^^^--IMPORTANT STUFF--^^^^--IMPORTANT STUFF--^^^^IMPORTANT STUFF^^^^--IMPORTANT STUFF--^^^^//
 
 
-
-
-
 //vvvvvvvvvvvvvvvvvvvvvUNIMPORTANTvvvvvvvvvvvvvvvvvvvvvvvvv//
 
 //Call the cat API to get a random cat picture.
 //Also has some code to handle what happens if the picture isn't available.
+
+image.onerror = getImage();
 
 function getImage(){
 
@@ -433,11 +424,10 @@ function getImage(){
         getImage();
     });
 }
-
 //Logs the error message if the fetch request returns an error.
 
-function handleErrors(response){
-    if(!response.ok){
+function handleErrors(response) {
+    if (!response.ok) {
         throw Error(response.statusText);
     }
     return response;
@@ -445,8 +435,8 @@ function handleErrors(response){
 
 //Sets the dimension of the HTML canvas to the original size of the image.
 
-function setCanvas(){
-    image2.src=image.src;
+function setCanvas() {
+    image2.src = image.src;
     canvas.width = image2.width;
     canvas.height = image2.height;
 }
@@ -454,7 +444,7 @@ function setCanvas(){
 //Finds the appropriate coordinates of the next image piece to draw based on
 //how many guesses the user has remaining. Then draw the image.
 
-function setGameImage(){
+function setGameImage() {
     let imageX = imagePieces[guessesRemaining][0];
     let imageY = imagePieces[guessesRemaining][1];
     drawImage(imageX, imageY);
@@ -465,6 +455,6 @@ function drawImage(x, y) {
     let pieceHeight = Math.round(canvas.height / 2);
     let pieceWidth = Math.round(canvas.width / 3);
 
-    ctx.drawImage(image, (x*pieceWidth), (y*pieceHeight), pieceWidth, pieceHeight, (x*pieceWidth), (y*pieceHeight), pieceWidth, pieceHeight);
+    ctx.drawImage(image, (x * pieceWidth), (y * pieceHeight), pieceWidth, pieceHeight, (x * pieceWidth), (y * pieceHeight), pieceWidth, pieceHeight);
 }
 //^^^^^^^^^^^^^^^^^^^UNIMPORTANT^^^^^^^^^^^^^^^^^^^^^^//
